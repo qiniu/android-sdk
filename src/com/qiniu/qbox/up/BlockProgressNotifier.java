25ce42e196a863e037ec63df3178bb2ee471bf2d
Cheney <me@chenye.org>
2013-03-28 00:44:03 +0800

    remove old file

--- src/com/qiniu/qbox/up/BlockProgressNotifier.java
+++
@@ -1,8 +0,0 @@
-package com.qiniu.qbox.up;
-
-/**
- * Notifies the progress of a block upload.
- */
-public interface BlockProgressNotifier {
-	void notify(int blockIndex, BlockProgress progress);
-}
