9d828dedeac57946f6de6eeb986e8b99ed4120bb
Cheney <me@chenye.org>
2013-04-26 17:06:06 +0800

    remove normal upload

--- src/com/qiniu/demo/MyActivity.java
+++ src/com/qiniu/demo/MyActivity.java
@@ -22,18 +22,14 @@ import org.json.JSONObject;
 
 public class MyActivity extends Activity implements View.OnClickListener{
 
-	public static final int PICK_PICTURE = 0;
-	public static final int PICK_PICTURE_RESUMABLE = 1;
+	public static final int PICK_PICTURE_RESUMABLE = 0;
 
 	// 在七牛绑定的对应bucket的域名. 可以到这里绑定 https://dev.qiniutek.com/buckets
 	public static String domain = "";
 	public static String bucketName = "";
-
 	// upToken 这里需要自行获取. SDK 将不实现获取过程.
-
 	public static final String UP_TOKEN = "";
 
-	private Button btnUpload;
 	private Button btnResumableUpload;
 	private EditText editKey;
 	private ProgressBar progressBar;
@@ -50,9 +46,6 @@ public class MyActivity extends Activity implements View.OnClickListener{
 	 * 初始化控件
 	 */
 	private void initWidget() {
-		btnUpload = (Button) findViewById(R.id.button);
-		btnUpload.setOnClickListener(this);
-
 		editKey = (EditText) findViewById(R.id.editText);
 		editKey.setText("android_sdk_demo");
 
@@ -60,18 +53,18 @@ public class MyActivity extends Activity implements View.OnClickListener{
 		btnResumableUpload.setOnClickListener(this);
 
 		progressBar = (ProgressBar) findViewById(R.id.progressBar);
+		progressBar.setMax(100);
+		progressBar.setProgress(0);
 	}
 
 	/**
-	 * 执行上传
+	 * 断点续上传
 	 * @param uri
 	 */
-	private void doUpload(Uri uri) {
+	private void doResumableUpload(Uri uri) {
 		final String key = editKey.getText().toString();
-		PutExtra extra = new PutExtra(bucketName);
-		extra.mimeType = "image/png";
 
-		IO.putFile(this, UP_TOKEN, key, uri, extra, new JSONObjectRet() {
+		ResumableIO.putFile(this, UP_TOKEN, key, uri, getPutExtra(uri), new JSONObjectRet() {
 			@Override
 			public void onSuccess(JSONObject resp) {
 				String hash;
@@ -94,17 +87,11 @@ public class MyActivity extends Activity implements View.OnClickListener{
 		});
 	}
 
-	/**
-	 * 断点许上传
-	 * @param uri
-	 */
-	private void doResumableUpload(Uri uri) {
-		final String key = editKey.getText().toString();
-		RputExtra extra = new RputExtra(bucketName);
-		extra.mimeType = "application/png";
+	private RputExtra getPutExtra(Uri uri) {
 		final long fsize = Utils.getSizeFromUri(this, uri);
-		progressBar.setMax(100);
-		progressBar.setProgress(0);
+
+		RputExtra extra = new RputExtra(bucketName);
+		extra.mimeType = "image/png";
 		extra.notify = new RputNotify() {
 			long uploaded = 0;
 			@Override
@@ -114,39 +101,11 @@ public class MyActivity extends Activity implements View.OnClickListener{
 				progressBar.setProgress(progress);
 			}
 		};
-
-
-		ResumableIO.putFile(this, UP_TOKEN, key, uri, extra, new JSONObjectRet() {
-			@Override
-			public void onSuccess(JSONObject resp) {
-				String hash;
-				try {
-					hash = resp.getString("hash");
-				} catch (Exception ex) {
-					toast(ex.getMessage());
-					return;
-				}
-				toast("上传成功! 正在跳转到浏览器查看效果 \nhash:" + hash);
-				String redirect = domain + "/" + key;
-				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(redirect));
-				startActivity(intent);
-			}
-
-			@Override
-			public void onFailure(Exception ex) {
-				toast("错误: " + ex.getMessage());
-			}
-		});
+		return extra;
 	}
 
 	@Override
 	public void onClick(View view) {
-		if (view.equals(btnUpload)) {
-			Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-			startActivityForResult(i, PICK_PICTURE);
-			return;
-		}
-
 		if (view.equals(btnResumableUpload)) {
 			Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 			startActivityForResult(i, PICK_PICTURE_RESUMABLE);
@@ -157,11 +116,6 @@ public class MyActivity extends Activity implements View.OnClickListener{
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode != RESULT_OK) return;
-
-		if (requestCode == PICK_PICTURE) {
-			doUpload(data.getData());
-			return;
-		}
 
 		if (requestCode == PICK_PICTURE_RESUMABLE) {
 			doResumableUpload(data.getData());
