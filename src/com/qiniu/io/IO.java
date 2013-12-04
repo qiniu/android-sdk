894c8f092c095a94ed3be7768d12d8c4eb8f7874
Cheney <me@chenye.org>
2013-12-04 20:03:58 +0800

    fix

--- src/com/qiniu/io/IO.java
+++ src/com/qiniu/io/IO.java
@@ -29,11 +29,11 @@ public class IO {
 	}
 
 	private static Client defaultClient() {
-		if (mClient != null && System.currentTimeMillis() - mClientUseTime > 60 * 1000) {
+		if (mClient != null && System.currentTimeMillis() - mClientUseTime > 60 * 1000) { // 1 minute
 			mClient.close();
 			mClient = null;
 		}
-		if (mClient == null) { // 1 minute
+		if (mClient == null) {
 			mClient = Client.defaultClient();
 		}
 		mClientUseTime = System.currentTimeMillis();
