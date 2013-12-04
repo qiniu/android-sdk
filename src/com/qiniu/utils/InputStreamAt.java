95703fc097c8b53053eda938753b38fb6144f640
Cheney <me@chenye.org>
2013-12-04 21:35:45 +0800

    use copyOfRange

--- src/com/qiniu/utils/InputStreamAt.java
+++ src/com/qiniu/utils/InputStreamAt.java
@@ -151,9 +151,18 @@ public class InputStreamAt implements Closeable {
 		}
 
 		if (totalRead != data.length) {
-			data = Arrays.copyOfRange(data, 0, totalRead);
+			data = copyOfRange(data, 0, totalRead);
 		}
 		return data;
+	}
+
+	public static byte[] copyOfRange(byte[] original, int from, int to) {
+		int newLength = to - from;
+		if (newLength < 0) throw new IllegalArgumentException(from + " > " + to);
+		byte[] copy = new byte[newLength];
+		System.arraycopy(original, from, copy, 0,
+		Math.min(original.length - from, newLength));
+	    return copy;
 	}
 
 	@Override
