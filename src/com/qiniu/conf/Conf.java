fcd674abc14087b133b71328afe3bc4f77fa0fe2
longbai <slongbai@gmail.com>
2014-07-15 00:53:54 +0800

    format

--- src/com/qiniu/conf/Conf.java
+++ src/com/qiniu/conf/Conf.java
@@ -2,7 +2,7 @@ package com.qiniu.conf;
 
 public class Conf {
     public static final String VERSION = "6.0.4";
-	public static String UP_HOST = "http://upload.qiniu.com";
+    public static String UP_HOST = "http://upload.qiniu.com";
     public static String UP_HOST2 = "http://up.qbox.me";
 
     public static String getUserAgent() {
