1e5a712a22bf8041d7414b1360210de0f066932a
longbai <slongbai@gmail.com>
2014-07-22 22:42:25 +0800

    add client id

--- src/com/qiniu/conf/Conf.java
+++ src/com/qiniu/conf/Conf.java
@@ -1,11 +1,22 @@
 package com.qiniu.conf;
 
+import java.util.Random;
+
 public class Conf {
     public static final String VERSION = "6.0.5";
     public static String UP_HOST = "http://upload.qiniu.com";
     public static String UP_HOST2 = "http://up.qiniu.com";
 
+    private static String id = genId();
+
     public static String getUserAgent() {
-        return  "QiniuAndroid/" + VERSION + " (" + android.os.Build.VERSION.RELEASE + "; " + android.os.Build.MODEL+ ")";
+        return  "QiniuAndroid/" + VERSION + " (" + android.os.Build.VERSION.RELEASE + "; "
+            + android.os.Build.MODEL+ "; " + id +")";
+    }
+
+    private static String genId(){
+        Random r = new Random();
+        int rnum = r.nextInt(999);
+        return System.currentTimeMillis() + "" + rnum;
     }
 }
