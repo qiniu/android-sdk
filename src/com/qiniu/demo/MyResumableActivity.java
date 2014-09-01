28176c6f04c752da32a775cdc5562d3df131fc57
simon-liubin <simon.liubin@gmail.com>
2014-09-01 13:45:23 +0800

    demo

--- src/com/qiniu/demo/MyResumableActivity.java
+++ src/com/qiniu/demo/MyResumableActivity.java
@@ -1,6 +1,7 @@
 package com.qiniu.demo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,19 +9,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-import org.json.JSONException;
-import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import com.qiniu.R;
-import com.qiniu.auth.JSONObjectRet;
-import com.qiniu.resumableio.PutExtra;
+import com.qiniu.auth.Authorizer;
 import com.qiniu.resumableio.ResumableIO;
+import com.qiniu.resumableio.SliceUploadTask.Block;
+import com.qiniu.rs.CallBack;
+import com.qiniu.rs.CallRet;
+import com.qiniu.rs.PutExtra;
+import com.qiniu.rs.UploadCallRet;
+import com.qiniu.rs.UploadTaskExecutor;
 import com.qiniu.utils.QiniuException;
 
 public class MyResumableActivity extends Activity implements View.OnClickListener {
+	
+	 private Authorizer auth = new Authorizer();
+	{
+			auth.setUploadToken(MyActivity.uptoken); 
+	}
+	
 	private ProgressBar pb;
 	private Button start;
 	private Button stop;
@@ -50,55 +61,79 @@ public class MyResumableActivity extends Activity implements View.OnClickListene
 			return;
 		}
 		if (view.equals(stop)) {
-			if (uploadUri == null) {
-				Toast.makeText(this, "还没开始任务", 20).show();
-				return;
+			if(executor != null && uploading){
+				executor.cancel();
+				uploading = false;
+				clean();
+				stop.setText("PLAY");
+			}else{
+				stop.setText("STOP");
+				hint.setText("连接中");
+				doResumableUpload(uploadUri, mExtra);
 			}
-			if (taskId >= 0) {
-				ResumableIO.stop(taskId);
-				stop.setText("开始");
-				hint.setText("暂停");
-				taskId = -1;
-				return;
-			}
-			stop.setText("暂停");
-			hint.setText("连接中");
-			doResumableUpload(uploadUri, mExtra);
-			return;
 		}
 	}
-	int taskId = -1;
+	
+	private void clean(){
+		executor = null;
+	}
+	
+	volatile boolean uploading = false;
+	UploadTaskExecutor executor;
 	Uri uploadUri;
-	PutExtra mExtra;
+	PutExtra mExtra = new PutExtra();
 
 	public void doResumableUpload(final Uri uri, PutExtra extra) {
+		uploadUri = uri;
+		final MyBlockRecord record = MyBlockRecord.genFromUri(this, uri);
+		
 		hint.setText("连接中");
 		String key = null;
-		String token = "<token>";
-		extra.params = new HashMap<String, String>();
-		extra.params.put("x:a", "bb");
-		taskId = ResumableIO.putFile(this, token, key, uri, extra, new JSONObjectRet() {
+		if(extra != null){
+			extra.params = new HashMap<String, String>();
+			extra.params.put("x:a", "bb");
+		}
+		List<Block> blks = record.loadBlocks();
+		String s = "blks.size(): " + blks.size() + " ==> ";
+		 for(Block blk : blks ){
+			 s += blk.getIdx() + ", ";
+		 }
+		 final String pre = s + "\r\n";
+		 uploading = true;
+		executor = ResumableIO.putFile(this, auth, key, uri, extra, blks, new CallBack() {
 			@Override
-			public void onSuccess(JSONObject obj) {
-				hint.setText("上传成功: " + obj.optString("key", ""));
+			public void onSuccess(UploadCallRet ret) {
+				uploading = false;
+				String key = ret.getKey();
+				String redirect = "http://" + MyActivity.bucketName + ".qiniudn.com/" + key;
+				String redirect2 ="http://" + MyActivity.bucketName + ".u.qiniudn.com/" + key;
+				hint.setText(pre + "上传成功! ret: " + ret.toString() + "  \r\n可到" + redirect + " 或  " + redirect2 + " 访问");
+				record.removeBlocks();
+				clean();
 			}
 
 			@Override
 			public void onProcess(long current, long total) {
-				float percent = (float) (current*10000/total) / 100;
-				hint.setText("上传中: " + percent + "%");
+				int percent = (int)(current*100/total);
+				hint.setText(pre + "上传中: " + current + "/" + total + "  " + current/1024 + "K/" + total/1024 + "K; " + percent + "%");
+				//int i = 3/0;
 				pb.setProgress((int) percent);
 			}
 
 			@Override
-			public void onPause(Object tag) {
-				uploadUri = uri;
-				mExtra = (PutExtra) tag;
+			public void onBlockSuccess(Block blk){
+				record.saveBlock(blk);
 			}
 
 			@Override
-			public void onFailure(QiniuException ex) {
-				hint.setText(ex.getMessage());
+			public void onFailure(CallRet ret, QiniuException ex) {
+				uploading = false;
+				clean();
+				hint.setText(pre + "错误: " + (ret != null ? ret.toString() : ex.toString()));
+				ex.printStackTrace();
+				if(ex.reason != null){
+					ex.reason.printStackTrace();
+				}
 			}
 		});
 	}
@@ -107,21 +142,6 @@ public class MyResumableActivity extends Activity implements View.OnClickListene
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode != RESULT_OK) return;
 		PutExtra e = new PutExtra();
-		e.notify = new PutExtra.INotify() {
-			@Override
-			public void onSuccessUpload(PutExtra ex) {
-				if (ex.isFinishAll()) return;
-				JSONObject json;
-				try {
-					json = ex.toJSON();
-				} catch (JSONException e1) {
-					e1.printStackTrace();
-					return;
-				}
-				// store to disk
-				// restore PutExtra by new PutExtra(JSONObject);
-			}
-		};
 		doResumableUpload(data.getData(), e);
 	}
 
@@ -135,4 +155,45 @@ public class MyResumableActivity extends Activity implements View.OnClickListene
 			ex.printStackTrace();
 		}
 	}
+	
+	static class MyBlockRecord{
+		private static HashMap<String, List<Block>> records = new HashMap<String, List<Block>>();
+		
+		public static MyBlockRecord genFromUri(Context context, Uri uri){
+			String id = uri.getPath() + context.toString();
+			return new MyBlockRecord(id);
+		}
+		
+		private final String id;
+		private List<Block> lastUploadBlocks;
+		public MyBlockRecord(String id){
+			this.id = id;
+		}
+		
+		public List<Block> loadBlocks() {
+			if(lastUploadBlocks == null){
+				List<Block> t = records.get(id);
+				if(t == null){
+					t = new ArrayList<Block>();
+					records.put(id, t);
+				}
+				lastUploadBlocks = t;
+			}
+			return lastUploadBlocks; 
+		}
+		
+		/**
+		 * @param blk 断点记录， 以4M为一个断点单元
+		 */
+		public void saveBlock(Block blk){
+			if(lastUploadBlocks != null){
+				lastUploadBlocks.add(blk);
+			}
+		}
+		
+		public void removeBlocks(){
+			records.remove(id);
+		}
+	}
+	
 }
