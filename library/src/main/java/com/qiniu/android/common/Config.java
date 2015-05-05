9924854f8d62d04f8644899381435b7baa58d4d7
simon-liubin <simon.liubin@gmail.com>
2015-05-05 11:34:57 +0800

    移除config中final字段

--- library/src/main/java/com/qiniu/android/common/Config.java
+++ library/src/main/java/com/qiniu/android/common/Config.java
@@ -7,53 +7,47 @@ public final class Config {
     /**
      * 默认上传服务器
      */
-    public static final String UP_HOST = "upload.qiniu.com";
+    public static String UP_HOST = "upload.qiniu.com";
 
     /**
      * 备用上传服务器，当默认服务器网络链接失败时使用
      */
-    public static final String UP_HOST_BACKUP = "up.qiniu.com";
+    public static String UP_HOST_BACKUP = "up.qiniu.com";
 
     /**
      * 备用上传服务器，当默认服务器网络链接失败时使用
      */
-    public static final String UP_IP_BACKUP = "183.136.139.16";
+    public static String UP_IP_BACKUP = "183.136.139.16";
 
     /**
-     * 默认上传服务器
+     * 断点上传时的分片大小(可根据网络情况适当调整，小于等于4M)
      */
-    public static String defaultUpHost = UP_HOST;
-
-
-    /**
-     * 断点上传时的分片大小(可根据网络情况适当调整)
-     */
-    public static final int CHUNK_SIZE = 256 * 1024;
+    public static int CHUNK_SIZE = 256 * 1024;
 
     /**
-     * 断点上传时的分块大小(默认的分块大小, 不建议改变)
+     * 断点上传时的分块大小(默认的分块大小, 不允许改变)
      */
     public static final int BLOCK_SIZE = 4 * 1024 * 1024;
 
     /**
      * 如果文件大小大于此值则使用断点上传, 否则使用Form上传
      */
-    public static final int PUT_THRESHOLD = 512 * 1024;
+    public static int PUT_THRESHOLD = 512 * 1024;
 
     /**
      * 连接超时时间(默认10s)
      */
-    public static final int CONNECT_TIMEOUT = 10 * 1000;
+    public static int CONNECT_TIMEOUT = 10 * 1000;
 
     /**
      * 回复超时时间(默认30s)
      */
-    public static final int RESPONSE_TIMEOUT = 30 * 1000;
+    public static int RESPONSE_TIMEOUT = 30 * 1000;
 
     /**
      * 上传失败重试次数
      */
-    public static final int RETRY_MAX = 5;
+    public static int RETRY_MAX = 5;
 
     public static final String UTF_8 = "utf-8";
 
