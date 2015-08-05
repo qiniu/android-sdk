11f7954a832bd6a6ab103b1070402096a372bcd2
longbai <slongbai@gmail.com>
2015-08-06 07:33:23 +0800

    fixed file handle leak

--- library/src/main/java/com/qiniu/android/storage/Configuration.java
+++ library/src/main/java/com/qiniu/android/storage/Configuration.java
@@ -109,7 +109,7 @@ public final class Configuration {
             IResolver r1 = AndroidDnsServer.defaultResolver();
             IResolver r2 = null;
             try {
-                r2 = new Resolver(InetAddress.getByName("114.114.115.115"));
+                r2 = new Resolver(InetAddress.getByName("223.6.6.6"));
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
