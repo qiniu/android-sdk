47d284174651c23cd88cbc3b9dbf8985d40c9a63
longbai <slongbai@gmail.com>
2016-01-29 13:29:29 +0800

    handle exception e in runable

--- library/src/main/java/com/qiniu/android/utils/AsyncRun.java
+++ library/src/main/java/com/qiniu/android/utils/AsyncRun.java
@@ -7,8 +7,17 @@ import android.os.Looper;
  * Created by bailong on 14/10/22.
  */
 public final class AsyncRun {
-    public static void run(Runnable r) {
+    public static void run(final Runnable r) {
         Handler h = new Handler(Looper.getMainLooper());
-        h.post(r);
+        h.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    r.run();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
