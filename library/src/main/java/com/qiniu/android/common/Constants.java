28f7bffbc2ce846169d72ecb978653df204c4bc3
jemygraw <jemygraw@gmail.com>
2018-03-12 15:18:24 +0800

    update changeling to publish new version

--- library/src/main/java/com/qiniu/android/common/Constants.java
+++ library/src/main/java/com/qiniu/android/common/Constants.java
@@ -2,7 +2,7 @@ package com.qiniu.android.common;
 
 
 public final class Constants {
-    public static final String VERSION = "7.3.10";
+    public static final String VERSION = "7.3.11";
 
     public static final String UTF_8 = "utf-8";
 }
