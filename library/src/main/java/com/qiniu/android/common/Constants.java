b07f731d8b761d7d6b769b81c6047fe8cfe914bd
YangSen-qn <yangsen@qiniu.com>
2020-09-25 14:22:11 +0800

    change version info

--- library/src/main/java/com/qiniu/android/common/Constants.java
+++ library/src/main/java/com/qiniu/android/common/Constants.java
@@ -2,7 +2,7 @@ package com.qiniu.android.common;
 
 
 public final class Constants {
-    public static final String VERSION = "8.0.2";
+    public static final String VERSION = "8.0.3";
 
     public static final String UTF_8 = "utf-8";
 }
