bcf510d057e5690832c4ecb8f2aeef4145456c74
YangSen-qn <yangsen@qiniu.com>
2021-01-14 11:05:53 +0800

    change connect check test case

--- library/src/androidTest/java/com/qiniu/android/ConnectCheckTest.java
+++ library/src/androidTest/java/com/qiniu/android/ConnectCheckTest.java
@@ -7,7 +7,7 @@ public class ConnectCheckTest extends BaseTest {
 
     public void testCheck() {
 
-        int maxCount = 1000;
+        int maxCount = 100;
         int successCount = 0;
         for (int i = 0; i < maxCount; i++) {
             if (ConnectChecker.check()) {
@@ -20,7 +20,7 @@ public class ConnectCheckTest extends BaseTest {
 
     public void testCustomCheckHosts() {
         GlobalConfiguration.getInstance().connectCheckURLStrings = new String[]{"https://www.baidu.com"};
-        int maxCount = 1000;
+        int maxCount = 100;
         int successCount = 0;
         for (int i = 0; i < maxCount; i++) {
             if (ConnectChecker.check()) {
@@ -33,7 +33,7 @@ public class ConnectCheckTest extends BaseTest {
 
     public void testNotConnected() {
         GlobalConfiguration.getInstance().connectCheckURLStrings = new String[]{"https://www.test1.com", "https://www.test2.com"};
-        int maxCount = 1000;
+        int maxCount = 100;
         int successCount = 0;
         for (int i = 0; i < maxCount; i++) {
             if (ConnectChecker.check()) {
