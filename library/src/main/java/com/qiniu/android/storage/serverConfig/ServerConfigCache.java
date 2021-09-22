0448157ee942d5a2cdb220dbfcf771b4a54f832c
YangSen-qn <yangsen@qiniu.com>
2021-09-22 16:12:14 +0800

    change server config disk cache

--- library/src/main/java/com/qiniu/android/storage/serverConfig/ServerConfigCache.java
+++ library/src/main/java/com/qiniu/android/storage/serverConfig/ServerConfigCache.java
@@ -14,34 +14,23 @@ class ServerConfigCache {
     private static String kServerConfigDiskKey = "ServerConfig:v1.0.0";
     private static String kServerUserConfigDiskKey = "ServerUserConfig:v1.0.0";
 
-    private boolean isHandlingServerConfigDisk = false;
-    private boolean isHandlingServerUserConfigDisk = false;
     private Recorder recorder;
 
 
     ServerConfig getConfig() {
-        if (config == null) {
-            config = getConfigFromDish();
-        }
         return config;
     }
 
     void setConfig(ServerConfig config) {
         this.config = config;
-        setConfigToDisk(config);
     }
 
-    private ServerConfig getConfigFromDish() {
+    ServerConfig getConfigFromDisk() {
         setupRecorder();
-
+        byte[] configData = null;
         synchronized (this) {
-            isHandlingServerConfigDisk = true;
+            configData = recorder.get(kServerConfigDiskKey);
         }
-        byte[] configData = recorder.get(kServerConfigDiskKey);
-        synchronized (this) {
-            isHandlingServerConfigDisk = false;
-        }
-
         if (configData == null) {
             return null;
         }
@@ -50,50 +39,41 @@ class ServerConfigCache {
         try {
             configJson = new JSONObject(new String(configData));
         } catch (Exception ignored) {
+            synchronized (this) {
+                recorder.del(kServerConfigDiskKey);
+            }
             return null;
         }
         return new ServerConfig(configJson);
     }
 
-    private void setConfigToDisk(ServerConfig config) {
+    void saveConfigToDisk(ServerConfig config) {
         if (config == null || config.getInfo() == null) {
             return;
         }
 
-        synchronized (this) {
-            if (isHandlingServerConfigDisk) {
-                return;
-            }
-        }
-
         setupRecorder();
-        recorder.set(kServerConfigDiskKey, config.getInfo().toString().getBytes());
+        synchronized (this) {
+            recorder.set(kServerConfigDiskKey, config.getInfo().toString().getBytes());
+        }
     }
 
 
     ServerUserConfig getUserConfig() {
-        if (userConfig == null) {
-            userConfig = getUserConfigFromDisk();
-        }
         return userConfig;
     }
 
     void setUserConfig(ServerUserConfig userConfig) {
         this.userConfig = userConfig;
-        setUserConfigToDisk(userConfig);
     }
 
-    private ServerUserConfig getUserConfigFromDisk() {
+    ServerUserConfig getUserConfigFromDisk() {
         setupRecorder();
 
+        byte[] configData = null;
         synchronized (this) {
-            isHandlingServerUserConfigDisk = true;
+            configData = recorder.get(kServerUserConfigDiskKey);
         }
-        byte[] configData = recorder.get(kServerUserConfigDiskKey);
-        synchronized (this) {
-            isHandlingServerUserConfigDisk = false;
-        }
-
         if (configData == null) {
             return null;
         }
@@ -102,24 +82,23 @@ class ServerConfigCache {
         try {
             configJson = new JSONObject(new String(configData));
         } catch (Exception ignored) {
+            synchronized (this) {
+                recorder.del(kServerUserConfigDiskKey);
+            }
             return null;
         }
         return new ServerUserConfig(configJson);
     }
 
-    private void setUserConfigToDisk(ServerUserConfig userConfig) {
+    void setUserConfigToDisk(ServerUserConfig userConfig) {
         if (userConfig == null || userConfig.getInfo() == null) {
             return;
         }
 
-        synchronized (this) {
-            if (isHandlingServerUserConfigDisk) {
-                return;
-            }
-        }
-
         setupRecorder();
-        recorder.set(kServerUserConfigDiskKey, userConfig.getInfo().toString().getBytes());
+        synchronized (this) {
+            recorder.set(kServerUserConfigDiskKey, userConfig.getInfo().toString().getBytes());
+        }
     }
 
     private synchronized void setupRecorder() {
--- library/src/main/java/com/qiniu/android/storage/serverConfig/ServerConfigMonitor.java
+++ library/src/main/java/com/qiniu/android/storage/serverConfig/ServerConfigMonitor.java
@@ -59,7 +59,13 @@ public class ServerConfigMonitor {
             return;
         }
 
-        final ServerConfig serverConfig = cache.getConfig();
+        if (cache.getConfig() == null) {
+            ServerConfig config = cache.getConfigFromDisk();
+            handleServerConfig(config);
+            cache.setConfig(config);
+        }
+
+        ServerConfig serverConfig = cache.getConfig();
         if (serverConfig == null || !serverConfig.isValid()) {
             ServerConfigSynchronizer.getServerConfigFromServer(new ServerConfigSynchronizer.ServerConfigHandler() {
                 @Override
@@ -68,85 +74,112 @@ public class ServerConfigMonitor {
                         return;
                     }
 
-                    // 清理 region 缓存
-                    ServerConfig.RegionConfig regionConfig = config.getRegionConfig();
-                    ServerConfig.RegionConfig oldRegionConfig = serverConfig != null ? serverConfig.getRegionConfig() : null;
-                    if (regionConfig != null) {
-                        if (oldRegionConfig != null && regionConfig.getClearId() > oldRegionConfig.getClearId() && regionConfig.getClearCache()) {
-                            AutoZone.clearCache();
-                        }
-                    }
-
-                    // dns 配置
-                    ServerConfig.DnsConfig dnsConfig = config.getDnsConfig();
-                    if (dnsConfig != null) {
-                        if (dnsConfig.getEnable() != null) {
-                            GlobalConfiguration.getInstance().isDnsOpen = dnsConfig.getEnable();
-                        }
-
-                        // 清理 dns 缓存
-                        ServerConfig.DnsConfig oldDnsConfig = serverConfig != null ? serverConfig.getDnsConfig() : null;
-                        if (oldDnsConfig != null && dnsConfig.getClearId() > oldDnsConfig.getClearId() && dnsConfig.getClearCache()) {
-                            try {
-                                DnsPrefetcher.getInstance().clearDnsCache();
-                            } catch (Exception ignored) {
-                            }
-                        }
-
-                        // udp 配置
-                        ServerConfig.UdpDnsConfig udpDnsConfig = dnsConfig.getUdpDnsConfig();
-                        if (udpDnsConfig != null) {
-                            if (udpDnsConfig.getEnable() != null) {
-                                GlobalConfiguration.getInstance().udpDnsEnable = udpDnsConfig.getEnable();
-                            }
-
-                            ServerConfig.DnsServer ipv4Servers = udpDnsConfig.getIpv4Server();
-                            if (ipv4Servers != null && ipv4Servers.getIsOverride()) {
-                                GlobalConfiguration.DefaultUdpDnsIpv4Servers = ipv4Servers.getServers();
-                            }
-
-                            ServerConfig.DnsServer ipv6Servers = udpDnsConfig.getIpv6Server();
-                            if (ipv6Servers != null && ipv6Servers.getIsOverride()) {
-                                GlobalConfiguration.DefaultUdpDnsIpv6Servers = ipv6Servers.getServers();
-                            }
-                        }
-
-                        // doh 配置
-                        ServerConfig.DohDnsConfig dohConfig = dnsConfig.getDohDnsConfig();
-                        if (dohConfig != null) {
-                            if (dohConfig.getEnable() != null) {
-                                GlobalConfiguration.getInstance().dohEnable = dohConfig.getEnable();
-                            }
-
-                            ServerConfig.DnsServer ipv4Servers = dohConfig.getIpv4Server();
-                            if (ipv4Servers != null && ipv4Servers.getIsOverride()) {
-                                GlobalConfiguration.DefaultUdpDnsIpv4Servers = ipv4Servers.getServers();
-                            }
-
-                            ServerConfig.DnsServer ipv6Servers = dohConfig.getIpv6Server();
-                            if (ipv6Servers != null && ipv6Servers.getIsOverride()) {
-                                GlobalConfiguration.DefaultDohIpv4Servers = ipv6Servers.getServers();
-                            }
-                        }
-                    }
-
+                    handleServerConfig(config);
                     cache.setConfig(config);
+                    cache.saveConfigToDisk(config);
                 }
             });
         }
 
+        if (cache.getUserConfig() == null) {
+            ServerUserConfig config = cache.getUserConfigFromDisk();
+            handleServerUserConfig(config);
+            cache.setUserConfig(config);
+        }
 
         ServerUserConfig serverUserConfig = cache.getUserConfig();
         if (serverUserConfig == null || !serverUserConfig.isValid()) {
             ServerConfigSynchronizer.getServerUserConfigFromServer(new ServerConfigSynchronizer.ServerUserConfigHandler() {
                 @Override
                 public void handle(ServerUserConfig config) {
-                    if (config.getNetworkCheckEnable() != null) {
-                        GlobalConfiguration.getInstance().connectCheckEnable = config.getNetworkCheckEnable();
+                    if (config == null) {
+                        return;
                     }
+
+                    handleServerUserConfig(config);
                     cache.setUserConfig(config);
+                    cache.setUserConfigToDisk(config);
                 }
             });
+        }
+    }
+
+    private void handleServerConfig(ServerConfig config) {
+        if (config == null) {
+            return;
+        }
+
+        final ServerConfig serverConfig = cache.getConfig();
+        // 清理 region 缓存
+        ServerConfig.RegionConfig regionConfig = config.getRegionConfig();
+        ServerConfig.RegionConfig oldRegionConfig = serverConfig != null ? serverConfig.getRegionConfig() : null;
+        if (regionConfig != null) {
+            if (oldRegionConfig != null && regionConfig.getClearId() > oldRegionConfig.getClearId() && regionConfig.getClearCache()) {
+                AutoZone.clearCache();
+            }
+        }
+
+        // dns 配置
+        ServerConfig.DnsConfig dnsConfig = config.getDnsConfig();
+        if (dnsConfig != null) {
+            if (dnsConfig.getEnable() != null) {
+                GlobalConfiguration.getInstance().isDnsOpen = dnsConfig.getEnable();
+            }
+
+            // 清理 dns 缓存
+            ServerConfig.DnsConfig oldDnsConfig = serverConfig != null ? serverConfig.getDnsConfig() : null;
+            if (oldDnsConfig != null && dnsConfig.getClearId() > oldDnsConfig.getClearId() && dnsConfig.getClearCache()) {
+                try {
+                    DnsPrefetcher.getInstance().clearDnsCache();
+                } catch (Exception ignored) {
+                }
+            }
+
+            // udp 配置
+            ServerConfig.UdpDnsConfig udpDnsConfig = dnsConfig.getUdpDnsConfig();
+            if (udpDnsConfig != null) {
+                if (udpDnsConfig.getEnable() != null) {
+                    GlobalConfiguration.getInstance().udpDnsEnable = udpDnsConfig.getEnable();
+                }
+
+                ServerConfig.DnsServer ipv4Servers = udpDnsConfig.getIpv4Server();
+                if (ipv4Servers != null && ipv4Servers.getIsOverride()) {
+                    GlobalConfiguration.DefaultUdpDnsIpv4Servers = ipv4Servers.getServers();
+                }
+
+                ServerConfig.DnsServer ipv6Servers = udpDnsConfig.getIpv6Server();
+                if (ipv6Servers != null && ipv6Servers.getIsOverride()) {
+                    GlobalConfiguration.DefaultUdpDnsIpv6Servers = ipv6Servers.getServers();
+                }
+            }
+
+            // doh 配置
+            ServerConfig.DohDnsConfig dohConfig = dnsConfig.getDohDnsConfig();
+            if (dohConfig != null) {
+                if (dohConfig.getEnable() != null) {
+                    GlobalConfiguration.getInstance().dohEnable = dohConfig.getEnable();
+                }
+
+                ServerConfig.DnsServer ipv4Servers = dohConfig.getIpv4Server();
+                if (ipv4Servers != null && ipv4Servers.getIsOverride()) {
+                    GlobalConfiguration.DefaultUdpDnsIpv4Servers = ipv4Servers.getServers();
+                }
+
+                ServerConfig.DnsServer ipv6Servers = dohConfig.getIpv6Server();
+                if (ipv6Servers != null && ipv6Servers.getIsOverride()) {
+                    GlobalConfiguration.DefaultDohIpv4Servers = ipv6Servers.getServers();
+                }
+            }
+        }
+    }
+
+    private void handleServerUserConfig(ServerUserConfig config) {
+        if (config == null) {
+            return;
+        }
+
+        if (config.getNetworkCheckEnable() != null) {
+            GlobalConfiguration.getInstance().connectCheckEnable = config.getNetworkCheckEnable();
         }
     }
 }
