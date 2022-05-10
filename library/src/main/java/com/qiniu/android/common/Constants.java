00fb178ea1639c6d1d87831e6a308eb96b845d38
yangsen <yangsen@qiniu.com>
2022-05-10 17:48:25 +0800

    change version

--- library/src/main/java/com/qiniu/android/common/Constants.java
+++ library/src/main/java/com/qiniu/android/common/Constants.java
@@ -2,11 +2,8 @@ package com.qiniu.android.common;
 
 
 public final class Constants {
-<<<<<<< HEAD
+
     public static final String VERSION = "8.4.2";
-=======
-    public static final String VERSION = "8.3.4";
->>>>>>> 8.3.4
 
     public static final String UTF_8 = "utf-8";
 }
