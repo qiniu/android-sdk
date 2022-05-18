6fce0a60f923abd93cfb65266e148361c1e2bec3
ZhaoMei <Mei-Zhao@users.noreply.github.com>
2022-05-18 19:06:29 +0800

    Update Constants.java

--- library/src/main/java/com/qiniu/android/common/Constants.java
+++ library/src/main/java/com/qiniu/android/common/Constants.java
@@ -3,7 +3,7 @@ package com.qiniu.android.common;
 
 public final class Constants {
 
-    public static final String VERSION = "8.4.2";
+    public static final String VERSION = "8.4.3";
 
     public static final String UTF_8 = "utf-8";
 }
