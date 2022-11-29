87bf7cf46e57fc91a13a0764b3eadbcd4ff032e2
Bai Long <slongbai@gmail.com>
2022-11-29 18:06:52 +0800

    update version

--- library/src/main/java/com/qiniu/android/common/Constants.java
+++ library/src/main/java/com/qiniu/android/common/Constants.java
@@ -2,7 +2,7 @@ package com.qiniu.android.common;
 
 
 public final class Constants {
-    public static final String VERSION = "8.5.0";
+    public static final String VERSION = "8.5.1";
 
     public static final String UTF_8 = "utf-8";
 }
