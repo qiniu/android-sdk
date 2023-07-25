6690c4b2ba31c7785d03b70e0038e32e94e44e98
yangsen <yang_sen_ysys@163.com>
2023-07-25 09:46:16 +0800

    change uc query retry host order (#501)

--- library/src/androidTest/java/com/qiniu/android/http/HttpTest.java
+++ library/src/androidTest/java/com/qiniu/android/http/HttpTest.java
@@ -163,8 +163,7 @@ public class HttpTest extends BaseTest {
             }
         }, 60);
 
-        assertTrue("status code:" + info.statusCode, info.statusCode == 418 || info.statusCode % 100 == 5);
-        assertEquals(418, info.statusCode);
+        assertTrue("status code:" + info.statusCode, info.statusCode == 418 || (info.statusCode / 100 == 5));
         assertNotNull(info.error);
     }
 
