8a0b3606b0e3aed60580e8ba9acaf9d779ab32da
YangSen-qn <yangsen@qiniu.com>
2023-10-11 18:28:32 +0800

    fix zone add createZonesInfo

--- library/src/main/java/com/qiniu/android/common/FixedZone.java
+++ library/src/main/java/com/qiniu/android/common/FixedZone.java
@@ -14,48 +14,82 @@ import java.util.List;
 public final class FixedZone extends Zone {
     /**
      * 华东机房
+     * <p>
+     * 废弃，使用 {@link FixedZone#createWithRegionId} 替换，regionId：z0
      */
+    @Deprecated
     public static final Zone zone0 = new FixedZone(new String[]{"upload.qiniup.com", "up.qiniup.com"},
             new String[]{"upload.qbox.me", "up.qbox.me"},
             "z0");
 
     /**
      * 华东浙江 2 机房
+     * <p>
+     * 废弃，使用 {@link FixedZone#createWithRegionId} 替换，regionId：cn-east-2
      */
+    @Deprecated
     public static final Zone zoneCnEast2 = new FixedZone(new String[]{"upload-cn-east-2.qiniup.com", "up-cn-east-2.qiniup.com"},
             null,
             "cn-east-2");
 
     /**
      * 华北机房
+     * <p>
+     * 废弃，使用 {@link FixedZone#createWithRegionId} 替换，regionId：z1
      */
+    @Deprecated
     public static final Zone zone1 = new FixedZone(new String[]{"upload-z1.qiniup.com", "up-z1.qiniup.com"},
             new String[]{"upload-z1.qbox.me", "up-z1.qbox.me"},
             "z1");
 
     /**
      * 华南机房
+     * <p>
+     * 废弃，使用 {@link FixedZone#createWithRegionId} 替换，regionId：z2
      */
+    @Deprecated
     public static final Zone zone2 = new FixedZone(new String[]{"upload-z2.qiniup.com", "up-z2.qiniup.com"},
             new String[]{"upload-z2.qbox.me", "up-z2.qbox.me"},
             "z2");
 
     /**
      * 北美机房
+     * <p>
+     * 废弃，使用 {@link FixedZone#createWithRegionId} 替换，regionId：na0
      */
+    @Deprecated
     public static final Zone zoneNa0 = new FixedZone(new String[]{"upload-na0.qiniup.com", "up-na0.qiniup.com"},
             new String[]{"upload-na0.qbox.me", "up-na0.qbox.me"},
             "na0");
 
     /**
      * 新加坡机房
+     * <p>
+     * 废弃，使用 {@link FixedZone#createWithRegionId} 替换，regionId：as0
      */
+    @Deprecated
     public static final Zone zoneAs0 = new FixedZone(new String[]{"upload-as0.qiniup.com", "up-as0.qiniup.com"},
             new String[]{"upload-as0.qbox.me", "up-as0.qbox.me"},
             "as0");
 
+    /**
+     * FixedZone 构造方法
+     * regionId 参考链接：https://developer.qiniu.com/kodo/1671/region-endpoint-fq
+     *
+     * @param regionId 根据区域 ID 创建 Zone
+     * @return Zone 实例
+     */
+    public static FixedZone createWithRegionId(String regionId) {
+        String[] upDomains = new String[]{
+                "upload-" + regionId + ".qiniup.com",
+                "up-" + regionId + ".qiniup.com"
+        };
+        return new FixedZone(upDomains, new String[]{}, "regionId");
+    }
+
     private ZonesInfo zonesInfo;
 
+    @Deprecated
     public static FixedZone localsZoneInfo() {
         FixedZone[] localsZone = new FixedZone[]{
                 (FixedZone) zone0, (FixedZone) zoneCnEast2,
