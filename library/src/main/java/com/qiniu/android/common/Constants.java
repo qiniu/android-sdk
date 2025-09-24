b3520742dd34ba6c56a21e965dab5679d9e8ef7b
YangSen-qn <yangsen@qiniu.com>
2025-09-24 10:22:46 +0800

    version to 8.9.2

--- library/src/main/java/com/qiniu/android/common/Constants.java
+++ library/src/main/java/com/qiniu/android/common/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     /**
      * SDK 版本号
      */
-    public static final String VERSION = "8.9.1";
+    public static final String VERSION = "8.9.2";
 
     /**
      * UTF-8 编码
